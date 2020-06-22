be2f41a1d296e7ce09e190e6a8516c602abf8e0a
kcsheng <kacysheng@mail.com>
2020-06-22 19:58:38 +1000

    template code in lib to override default erb

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  resources :books
+  resources :categories
+  resources :guides
+  resources :blogs
+  resources :posts
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
